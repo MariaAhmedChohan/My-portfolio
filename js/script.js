19172b261720841978786e3c49b4f878d01b6876
MariaAhmedChohan <mariaahmedchohan@gmail.com>
2024-09-27 23:21:11 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer","You Tuber","Blogger"],
+    typeSpeed: 100,
+    backDelay: 1000,
+    backSpeed: 100,
+    loop: true
+
+    
+})
